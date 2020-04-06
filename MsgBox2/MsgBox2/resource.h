486a33afb1ab66fa565d85442417edf99e01de5f
metalnom@hanmail.net <metalnom@hanmail.net>
2020-04-06 21:58:27 +0900

    MsgBox2

---
+++ MsgBox2/MsgBox2/resource.h
@@ -0,0 +1,24 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// MsgBox2.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MSGBOX2_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT_RESULT                 1000
+#define IDC_BUTTON_YN                   1001
+#define IDC_BUTTON_OC                   1002
+#define IDC_BUTTON_EXIT                 1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
