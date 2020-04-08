cc93f278d2c2be8a67253a87feb128042a8b84f9
metalnom@hanmail.net <metalnom@hanmail.net>
2020-04-08 17:38:36 +0900

    SDrawing

---
+++ SDrawing/SDrawing/SDrawingView.cpp
@@ -0,0 +1,148 @@
+﻿
+// SDrawingView.cpp: CSDrawingView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "SDrawing.h"
+#endif
+
+#include "SDrawingDoc.h"
+#include "SDrawingView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CSDrawingView
+
+IMPLEMENT_DYNCREATE(CSDrawingView, CView)
+
+BEGIN_MESSAGE_MAP(CSDrawingView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONDOWN()
+END_MESSAGE_MAP()
+
+// CSDrawingView 생성/소멸
+
+CSDrawingView::CSDrawingView()
+	: m_ptX(0)
+	, m_ptY(0)
+	, m_crColor(0)
+	, m_reRect(100, 100, 300, 300)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	m_crColor = BLACK_BRUSH;
+
+}
+
+CSDrawingView::~CSDrawingView()
+{
+}
+
+BOOL CSDrawingView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CSDrawingView 그리기
+
+void CSDrawingView::OnDraw(CDC* pDC)
+{
+	CSDrawingDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+	pDC->TextOutW(150, 50, _T("Hello, MFC"));
+	pDC->SelectStockObject(m_crColor);
+	pDC->Rectangle(m_reRect);
+}
+
+void CSDrawingView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CSDrawingView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CSDrawingView 진단
+
+#ifdef _DEBUG
+void CSDrawingView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CSDrawingView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CSDrawingDoc* CSDrawingView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CSDrawingDoc)));
+	return (CSDrawingDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CSDrawingView 메시지 처리기
+
+
+void CSDrawingView::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+
+	if (nFlags & MK_LBUTTON)
+	{
+		CClientDC dc(this);
+
+		dc.MoveTo(m_ptX, m_ptY);
+		dc.LineTo(point.x, point.y);
+
+		m_ptX = point.x;
+		m_ptY = point.y;
+	}
+	CView::OnMouseMove(nFlags, point);
+}
+
+
+void CSDrawingView::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	m_ptX = point.x;
+	m_ptY = point.y;
+
+	if (m_reRect.PtInRect(point))
+	{
+		if (m_crColor == BLACK_BRUSH)
+		{
+			m_crColor = WHITE_BRUSH;
+		}
+		else
+		{
+			m_crColor = GRAY_BRUSH;
+		}
+		InvalidateRect(m_reRect);
+	}
+
+	CView::OnLButtonDown(nFlags, point);
+}
---
+++ SDrawing/SDrawing/SDrawingView.h
@@ -0,0 +1,56 @@
+﻿
+// SDrawingView.h: CSDrawingView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CSDrawingView : public CView
+{
+protected: // serialization에서만 만들어집니다.
+	CSDrawingView();
+	DECLARE_DYNCREATE(CSDrawingView)
+
+// 특성입니다.
+public:
+	CSDrawingDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual void OnDraw(CDC* pDC);  // 이 뷰를 그리기 위해 재정의되었습니다.
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 구현입니다.
+public:
+	virtual ~CSDrawingView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	int m_ptX;
+	int m_ptY;
+	int m_crColor;
+	CRect m_reRect;
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+};
+
+#ifndef _DEBUG  // SDrawingView.cpp의 디버그 버전
+inline CSDrawingDoc* CSDrawingView::GetDocument() const
+   { return reinterpret_cast<CSDrawingDoc*>(m_pDocument); }
+#endif
+
